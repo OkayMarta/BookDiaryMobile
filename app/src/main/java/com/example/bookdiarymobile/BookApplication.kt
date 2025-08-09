2a3ed7e831461e90b5b7857615c0d20f858e76bf
Marta <152033888+OkayMarta@users.noreply.github.com>
2025-08-09 14:20:59 +0300

    UPD BookApplication.kt

--- app/src/main/java/com/example/bookdiarymobile/BookApplication.kt
+++ app/src/main/java/com/example/bookdiarymobile/BookApplication.kt
@@ -4,10 +4,12 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 /**
- * Основний клас додатку, що успадковується від Application.
- * Анотація @HiltAndroidApp запускає генерацію коду Hilt.
+ * Основний клас додатку, який слугує точкою входу та ініціалізує Hilt.
+ *
+ * Анотація [@HiltAndroidApp] вмикає механізм впровадження залежностей Hilt
+ * на рівні всього додатку. Вона запускає генерацію коду, який створює
+ * контейнер залежностей, що буде доступний протягом усього життєвого циклу
+ * додатку. Цей клас має бути зареєстрований в `AndroidManifest.xml`.
  */
 @HiltAndroidApp
-class BookApplication : Application() {
-    // Hilt ствоює БД і репозиторій...
-}
+class BookApplication : Application()
